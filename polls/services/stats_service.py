a8934bb56b9f28ae57a6e0328d5f3454af1e1dba
RaghadSalhab <raghadsalhab13@gmail.com>
2025-09-29 01:06:47 +0200

    some editions

--- polls/services/stats_service.py
+++ polls/services/stats_service.py
@@ -5,28 +5,16 @@ class StatsService:
 
     @staticmethod
     def get_top_question():
-        """
-        يرجع السؤال الأكثر تصويتًا
-        """
         return StatsRepository.top_voted_question()
 
     @staticmethod
     def get_question_votes(question_id):
-        """
-        يرجع مجموع الأصوات لسؤال معين
-        """
         return StatsRepository.question_votes(question_id)
 
     @staticmethod
     def get_top_choice():
-        """
-        يرجع الخيار الأكثر تصويتًا
-        """
         return StatsRepository.top_voted_choice()
 
     @staticmethod
     def list_questions_with_votes():
-        """
-        قائمة بكل الأسئلة مع مجموع أصواتها
-        """
         return StatsRepository.all_questions_with_votes()
