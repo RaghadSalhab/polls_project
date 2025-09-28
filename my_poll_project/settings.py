2fd9a19816414dff963e5778d26f5dc0b9b1af2a
RaghadSalhab <raghadsalhab13@gmail.com>
2025-09-29 01:16:37 +0200

    custom exception  handler

--- my_poll_project/settings.py
+++ my_poll_project/settings.py
@@ -52,6 +52,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated', 
     ),
+    'EXCEPTION_HANDLER': 'polls.utils.exception_handler.custom_exception_handler'
+
 
 
 
